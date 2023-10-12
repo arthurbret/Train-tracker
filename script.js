920fbadd860960797d5abd0fcb8690afabd39b91
Arthur <arthur.bret@edu.ece.fr>
2023-10-12 16:38:51 +0200

    Api SNCF en console log

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const input = document.querySelector('#input-gare');
 const bouton = document.querySelector('#recherche');
-const urlApiSncf = "https://api.sncf.com"
-const urlDataSncf = "https://data.sncf.com"
+const urlApiSncf = "https://api.sncf.com";
+const urlDataSncf = "https://data.sncf.com";
 const headers = {
     "Authorization" : "6d992b49-f48d-4c16-a560-6994483ae0b2"
 }
-let gareDemande = 0;
+let gareDemande;
 
 const getDataSncf = async (url) => {
     const data = await fetch(url)
@@ -13,14 +13,30 @@ const getDataSncf = async (url) => {
     return json
 }
 
-
+const getApiSncf = async (url) => {
+    const data = await fetch(url, {
+        headers: headers
+    })
+    const json = data.json()
+    return json
+}
 
 async function getUicCodeSncf(url, gare) {
     try {
-        const data = await getDataSncf(url + "/api/explore/v2.1/catalog/datasets/referentiel-gares-voyageurs/records?limit=20&refine=gare_alias_libelle_fronton%3A" + gare);
-        return data.results[0].uic_code;
+        const data = await getDataSncf(url + "/api/explore/v2.1/catalog/datasets/referentiel-gares-voyageurs/records?limit=20&refine=gare_alias_libelle_fronton%3A" + gare)
+        return data.results[0].uic_code
     } catch (error) {
-        console.error(error);
+        console.error(error)
+    }
+}
+
+async function getInfoApi(url, uicCode) {
+    try {
+        console.log(url + "/v1/coverage/sncf/stop_areas/stop_area%3ASNCF%3A" + uicCode + "/arrivals?")
+        const data = await getApiSncf(url + "/v1/coverage/sncf/stop_areas/stop_area%3ASNCF%3A" + uicCode + "/arrivals?")
+        return data
+    } catch (error) {
+        console.error(error)
     }
 }
 
@@ -28,9 +44,11 @@ bouton.addEventListener('click', async() => {
     gareDemande = document.getElementById('input-gare').value
     console.log(gareDemande)
     try {
-        const retour = await getUicCodeSncf(urlDataSncf, gareDemande);
-        console.log(retour);
+        const UIC = (await getUicCodeSncf(urlDataSncf, gareDemande)).slice(2)
+        console.log(UIC)
+        const dataApi = await getInfoApi(urlApiSncf, UIC)
+        console.log(dataApi)
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
 })
